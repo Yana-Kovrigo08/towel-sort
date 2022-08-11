0362f6a4ea048f6b4c316a1162f4c4d7b2d64975
Yana-Kovrigo08 <yana_kovrigo@mail.ru>
2022-08-11 15:48:28 +0100

    izmenen

--- src/index.js
+++ src/index.js
@@ -2,5 +2,14 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+
+  const half = matrix.length / 2
+  
+  // Base case or terminating case
+  if(matrix.length < 2){
+    return matrix 
+  }
+  
+  const left = matrix.splice(0, half)
+  return towelSort(towelSort(left),towelSort(matrix))
 }
